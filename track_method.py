220935f312b15b70064fb3a279808767f7dc0ce7
adriacustico <43962200+adriacustico@users.noreply.github.com>
2022-03-02 13:41:56 -0300

    Decorator activator

---
+++ track_method.py
@@ -0,0 +1,78 @@
+class Track_method:
+    def partialize_df(df, index, key = 'aircraft'):
+        ind = pd.DataFrame(index, columns=['Flight_ID'])
+        partial_df = pd.json_normalize(df.loc[:,key])
+        return pd.concat([ind,partial_df], axis=1)
+    def flight_operation(df, index):
+        airport_list = partialize_df(df, index, key = 'airport').dropna(axis =1, how='all')
+        arrival = airport_list[airport_list['destination.code.icao'].isin(['SCEL'])]
+        from_north = arrival[arrival['origin.position.latitude'] > arrival['destination.position.latitude']]
+        from_south = arrival[arrival['origin.position.latitude'] < arrival['destination.position.latitude']]
+        departure = airport_list[airport_list['origin.code.icao'].isin(['SCEL'])]
+        to_north = departure[departure['origin.position.latitude'] < departure['destination.position.latitude']]
+        to_south = departure[departure['origin.position.latitude'] > departure['destination.position.latitude']]
+        overflight = airport_list[~airport_list['destination.code.icao'].isin(['SCEL']) & ~airport_list['origin.code.icao'].isin(['SCEL'])]
+        return [from_north['Flight_ID'], from_south['Flight_ID'], to_north['Flight_ID'], to_south['Flight_ID'], overflight['Flight_ID']]
+
+    def uuttmm(fila):
+        [Easting, Northing, zone_number, zone_letter] = utm.from_latlon(fila['lat'], fila['lng'])
+        return (Easting, Northing, zone_number, zone_letter)
+    # Function to create a plotting area in xN meters from center point (Easting, Northing)
+    def plotting_area(Easting, Northing, xN):
+        east_left = Easting - xN
+        east_right = Easting + xN
+        north_up = Northing + xN
+        north_down = Northing - xN
+        return (east_left, east_right, north_up, north_down)
+
+    def iter_df(func):
+        def wrapper(df, index, key='trail'): #Parameters from the function plot_trail()
+            for i in index:
+                a=df.loc[i] # a is a row of the main dataframe
+                try:
+                    partial_df=pd.DataFrame(a[key])   
+                except ValueError:
+                    print('No data for flight ' + i)
+                    continue
+
+                if partial_df.empty:
+                    print("Empty trail: "+ i)
+                    continue
+                else:
+                    params = func(partial_df, ax)      
+            return params
+        return wrapper
+
+
+
+    ax = plt.figure().add_subplot(projection='3d')  
+    @iter_df
+    def plot_trail(partial_df, ax):
+        # Append columns with the UTM coordinates
+        coord_list = partial_df.apply(uuttmm, axis=1).to_list()
+        utm_coord=pd.DataFrame(coord_list, columns=['Easting', 'Northing', 'Zone_N', 'Zone_L'])
+        utm_trail_coord=pd.concat([partial_df, utm_coord], axis=1)
+        # Add data to plot
+        (Easting, Northing, zone_number, zone_letter) = utm.from_latlon(lat_long_airp[code]['Latitude'], 
+                                                                        lat_long_airp[code]['Longitude'])
+        (east_left, east_right, north_up, north_down)=plotting_area(Easting, Northing, 40000)
+        result_df=utm_trail_coord[(utm_trail_coord['Easting']>east_left) & (utm_trail_coord['Easting']<east_right) 
+                & (utm_trail_coord['Northing']>north_down) & (utm_trail_coord['Northing']<north_up)]
+        sc= ax.plot(result_df.Easting, result_df.Northing, result_df.alt*0.3048)
+        plot_params = {'sc':sc, 'east_left':east_left, 'east_right':east_right, 'north_up':north_up, 'north_down':north_down}
+        return plot_params
+
+    def flight_operation(df, index):
+        airport_list = partialize_df(df, index, key = 'airport').dropna(axis =1, how='all')
+        arrival = airport_list[airport_list['destination.code.icao'].isin(['SCEL'])]
+        from_north = arrival[arrival['origin.position.latitude'] > arrival['destination.position.latitude']]
+        from_south = arrival[arrival['origin.position.latitude'] < arrival['destination.position.latitude']]
+        departure = airport_list[airport_list['origin.code.icao'].isin(['SCEL'])]
+        to_north = departure[departure['origin.position.latitude'] < departure['destination.position.latitude']]
+        to_south = departure[departure['origin.position.latitude'] > departure['destination.position.latitude']]
+        overflight = airport_list[~airport_list['destination.code.icao'].isin(['SCEL']) & ~airport_list['origin.code.icao'].isin(['SCEL'])]
+        return [from_north['Flight_ID'], from_south['Flight_ID'], to_north['Flight_ID'], to_south['Flight_ID'], overflight['Flight_ID']]
+
+    def aircraft_model(df, index):
+        aircraft_list = partialize_df(df, index, key = 'aircraft')
+        return aircraft_list
